966ae62924c28cac5f6a7a1389692d77f9e069e3
rosstooley <31997234+rosstooley@users.noreply.github.com>
2018-05-15 15:11:12 +0100

    Added InterpanelData Object
    This allows data to be stored to be used when switching between frames.

---
+++ src/uk/ac/cam/ia/group14/util/InterpanelData.java
@@ -0,0 +1,13 @@
+package uk.ac.cam.ia.group14.util;
+
+public class InterpanelData {
+    private RegionID currentMountainRegion;
+
+    public RegionID getCurrentMountainRegion() {
+        return currentMountainRegion;
+    }
+
+    public void setCurrentMountainRegion(RegionID currentMountainRegion) {
+        this.currentMountainRegion = currentMountainRegion;
+    }
+}
